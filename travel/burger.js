9cc9edc1ccf5a0bf1852f4833e3a826e1015de76
MDViktor <milliondollarboymvd@gmail.com>
2022-07-13 02:27:58 +0300

    fix: burger3

--- travel/burger.js
+++ travel/burger.js
@@ -1,16 +1,22 @@
 const burgerMenu = document.querySelector(".burger_menu");
 const burgerButtonMenu = document.getElementById("burgerButton");
 const burgerExit = document.querySelector(".burger_exit_button");
+const accountExit = document.getElementById("accountexit");
 
 if (burgerMenu.classList.contains('menuAppearance')) {
   document.addEventListener('click', function(burgerButtonMenu) { 
     burgerMenu.classList.toggle('menuAppearance');
   })
 }
-else burgerButtonMenu.addEventListener("click", function() {
+else 
+burgerButtonMenu.addEventListener("click", function() {
     burgerMenu.classList.toggle('menuAppearance');        
 })
 
 burgerExit.addEventListener("click", function() {
+  burgerMenu.classList.toggle('menuAppearance')
+})
+
+accountExit.addEventListener("click", function() {
   burgerMenu.classList.toggle('menuAppearance')
 })
