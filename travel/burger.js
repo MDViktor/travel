22f340d7c43912de551c2492d49413a02e3b8053
MDViktor <milliondollarboymvd@gmail.com>
2022-07-21 01:32:58 +0300

    fix: burger

--- travel/burger.js
+++ travel/burger.js
@@ -2,21 +2,27 @@ const burgerMenu = document.querySelector(".burger_menu");
 const burgerButtonMenu = document.getElementById("burgerButton");
 const burgerExit = document.querySelector(".burger_exit_button");
 const accountExit = document.getElementById("accountexit");
+const navItem = document.querySelector(".nav_item");
+
 
 if (burgerMenu.classList.contains('menuAppearance')) {
-  document.addEventListener('click', function(burgerButtonMenu) { 
+  document.addEventListener('click', function() {
     burgerMenu.classList.toggle('menuAppearance');
   })
 }
-else 
-burgerButtonMenu.addEventListener("click", function() {
-    burgerMenu.classList.toggle('menuAppearance');        
-})
+else {burgerButtonMenu.addEventListener("click", function() {
+    burgerMenu.classList.toggle('menuAppearance');
+})}
 
 burgerExit.addEventListener("click", function() {
-  burgerMenu.classList.toggle('menuAppearance')
+  
+  burgerMenu.classList.toggle('menuAppearance');
 })
 
-accountExit.addEventListener("click", function() {
+// accountExit.addEventListener("click", function() {
+//   burgerMenu.classList.toggle('menuAppearance')
+// })
+
+navItem.addEventListener("click", function() {
   burgerMenu.classList.toggle('menuAppearance')
 })
