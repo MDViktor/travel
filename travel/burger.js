6e16512fffc9337fae3f36976c36a9e2c215f9f3
MDViktor <milliondollarboymvd@gmail.com>
2022-07-24 00:34:08 +0300

    add: click-close popup

--- travel/burger.js
+++ travel/burger.js
@@ -3,7 +3,7 @@ const burgerButtonMenu = document.getElementById("burgerButton");
 const burgerWrapper = document.querySelector(".burger_menu_wrapper");
 const loginButton = document.querySelector(".login");
 const popUpWrapper = document.querySelector(".pop_up_menu_wrapper");
-const popUpMenu = document.querySelector(".pop_up_menu ");
+const popUpMenu = document.querySelector(".pop_up_menu");
 
 
 burgerButtonMenu.addEventListener('click', function() {
@@ -22,10 +22,11 @@ loginButton.addEventListener('click', function() {
   popUpMenu.classList.toggle('menuAppearance');
 })
 
-// if (popUpMenu.classList.contains('menuAppearance')) {
-
-// }
-popUpWrapper.addEventListener('click', function() {
-  popUpWrapper.classList.toggle('damping');
-  popUpMenu.classList.toggle('menuAppearance');
-})
+const whereIclick = (event) => {
+  element = event.target;
+  if (element ===popUpWrapper) {
+    popUpWrapper.classList.toggle('damping');
+    popUpMenu.classList.toggle('menuAppearance');
+  }
+}
+popUpWrapper.addEventListener('click', whereIclick)
