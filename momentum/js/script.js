a75287b51e93a85a905c27f499c6fca1e9972ad5
MDViktor <milliondollarboymvd@gmail.com>
2022-08-15 00:31:39 +0300

    init: date

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,10 +1,19 @@
-let time = document.querySelector('.time');
+const time = document.querySelector('.time');
+const date = document.querySelector('.date');
 
 function showTime() {
     const date = new Date();
     const currentTime = date.toLocaleTimeString();
     time.textContent = currentTime;
+    showDate();
     setTimeout(showTime, 1000);
+}
+
+function showDate() {
+    const text = new Date();
+    const options = {weekday:'long', month: 'long', day: 'numeric'};
+    const currentDate = text.toLocaleDateString('en-US', options);
+    date.textContent = currentDate;
 }
 
 showTime();
